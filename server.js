5065fc14a781ccc69fd144bc06395b2e53734cd5
Skyler DeVaughn <skylerjoe@email.arizona.edu>
2023-11-18 18:00:58 -0700

    STUPID COOKIES

--- server.js
+++ server.js
@@ -35,6 +35,77 @@ const queueSchema = new Schema({
 let ip = "127.0.0.1";
 let port = "80";
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+let sessions = {};
+
+function authenticate(res, res, next) {
+    let c = req.cookies;
+    console.log(c);
+    if (c.login != undefined) {
+        if (sessions[c.login.username] != undefined &&
+            sessions[c.login.username].id == c.login.sessionID) {
+            
+            console.log(c);
+            next();
+        } else {
+            console.log("failed auth because user is invalid");
+            res.redirect("/login/login.html");
+        }
+    } else {
+        console.log("failed auth because cookue is wack man");
+        res.redirect("/login/login.html");
+    }
+}
+
+/**
+ * @param {username of the user who you are making the session for} username 
+ * adds a session
+ */
+function addSession(username) {
+    let sid = Math.floor(Math.random() * 100000000000);
+    let now = Date.now()
+    sessions[username] = {id: sid, time: now};
+    return sid;
+}
+/**
+ * remove sessions that have expired
+ */
+function removeSessions() {
+    let now = Date.now;
+    let usernames = Object.keys(sessions);
+    for (let i = 0; i < usernames.length; i++) {
+        let last = sessions[username[i]].time;
+        if (last + 20000000 < now) {
+            delete sessions[username[i]];
+        }
+    }
+}
+
+app.use("/tutor/", authenticate);
+setInterval(removeSessions, 2000);
+
+app.post("/tutor/login", (req, res) => {
+    let user = req.body;
+    // search database for username password combo put promise in p
+    let p = 0
+
+    p.then((results) => {
+        if (results.lenght == 0) {
+            res.end("could not find account");
+        } else {
+            console.log("Accound found: creating cookie");
+            let sid = addSession(user.usernname);
+            res.cookie(
+                "login",
+                {username: user.username, sessionID: sid},
+                {maxAge: 600000 * 3600}
+            );
+            res.end("SUCCESS");
+        }
+    });
+}
+
 
 app.listen(port, () => {
     console.log(`server running on http://${ip}:${port}`);
