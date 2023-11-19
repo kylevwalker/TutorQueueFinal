eb853ea1ec3bf16380e0b579ca03b9a2c7ddd457
raycauski <85911092+raycauski@users.noreply.github.com>
2023-11-19 00:14:34 -0700

    HTML pages

---
+++ TODO.md
@@ -0,0 +1,9 @@
+## Skallop:
+
+- Add authentication for both students and tutors to separate app into tutor and student pages
+- Students can access student pages but not tutor pages and vice versa
+
+
+## Kyle
+
+- HTML elements for buttons to navigate between common pages
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public_html"));
 
+let ip = "127.0.0.1";
+let port = "80";
+
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/TutorQueue");
 mongoose.connection.on("error", () => {
@@ -32,13 +35,15 @@ const queueSchema = new Schema({
     status: String,
 });
 
-let ip = "127.0.0.1";
-let port = "80";
+var Student = mongoose.model("Student", studentSchema);
+var Tutor = mongoose.model("Tutor", tutorSchema);
+var Queue = mongoose.model("Queue", queueSchema);
 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 let sessions = {};
 
+// TODO: Check if user is student or tutor to determine which pages are visible
 function authenticate(res, res, next) {
     let c = req.cookies;
     console.log(c);
@@ -59,7 +64,7 @@ function authenticate(res, res, next) {
 }
 
 /**
- * @param {username of the user who you are making the session for} username 
+ * @param username name of the user who you are making the session for 
  * adds a session
  */
 function addSession(username) {
@@ -85,8 +90,9 @@ function removeSessions() {
 app.use("/tutor/", authenticate);
 setInterval(removeSessions, 2000);
 
-app.post("/tutor/login", (req, res) => {
-    let user = req.body;
+/**Login as a Tutor or Student. If a tutor, send to tutor home page. If Student, send to student home. */
+app.post("/login", (req, res) => {
+    let user = req.body.user;
     // search database for username password combo put promise in p
     let p = 0
 
@@ -106,6 +112,7 @@ app.post("/tutor/login", (req, res) => {
     });
 });
 
+app.get("")
 
 app.listen(port, () => {
     console.log(`server running on http://${ip}:${port}`);
