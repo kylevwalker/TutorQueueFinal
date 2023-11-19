15b594b576e4b2c6b489bce03e35ae01ce1251a4
Skyler DeVaughn <skylerjoe@email.arizona.edu>
2023-11-18 17:06:41 -0700

    please work

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+app.use(express.json);
+app.use(express.static("public_html"));
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/TutorQueue");
+mongoose.connection.on("error", () => {
+    console.log("ERROR CONNECTING TO MONGODB");
+});
+
+let Schema = mongoose.Schema;
+const studentSchema = new Schema({
+    name: String,
+    password: String,
+    isTutor: Boolean,
+});
+
+const tutorSchema = new Schema({
+    name: String,
+    password: String,
+    tutorCoordinationRank: Number,
+    studentsHelped: Number,
+});
+
+const queueSchema = new Schema({
+    time: Number,
+    student: String,
+    tutor: String,
+    course: String,
+    desctiprion: String,
+    status: String,
+});
+
+let ip = "127.0.0.1";
+let port = "80";
+
+
+app.listen(port, () => {
+    console.log(`server runninging on http://${ip}:${port}`);
+});
